be58c2bd7799520ff37d09b529ecae4ea4c949fc
tano.vanlerberghe <tano.vanlerberghe@student.howest.be>
2024-11-05 09:57:19 +0100

    init

---
+++ src/views/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <div id="brand">
+      <img src="../assets/logo.png" alt="Edenscouts logo" />
+      <h1>Edenscouts</h1>
+    </div>
+    <div id="navigation"></div>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/deployments">Deployments</RouterLink>
+  </nav>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    border-bottom: 1px solid var(--orange-primary-color);
+    max-height: 7vh;
+  }
+
+  #brand {
+    display: flex;
+    align-items: center;
+  }
+
+  #navigation {
+    display: flex;
+    gap: 1rem;
+  }
+
+  nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    text-decoration: none;
+  }
+
+
+</style>
